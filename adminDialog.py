f7537d9178cce477ae616db5d07940fd59b4fad6
Thomas Lundgaard <thomaslundgaard@gmail.com>
2009-11-02 21:20:43 +0100

    adminauth dialog now working + changed the way the app keeps other apps from stealing focus (alt+tab) + only update ui when playlist or playing song actually changed

--- adminDialog.py
+++ adminDialog.py
@@ -6,24 +6,30 @@ class AdminDialog(QtGui.QDialog):
         QtGui.QDialog.__init__ (self,  parent)
         self.ui = Ui_AdminDialog ()
         self.ui.setupUi (self)
+        self.ui.infoLabel.hide()
+        self.resize(-1, -1)
         self.ui.pwdEdit.setFocus()
     
     def checkPwd(self):
-        if str(self.ui.pwdEdit.text()) == "kkk":
+         if str(self.ui.pwdEdit.text()) == "kkk":
+            self.close()
             return True
-        else:
+         else:
+            self.ui.pwdEdit.clear()
+            self.ui.infoLabel.setText("Invalid password - please try again:")
+            self.ui.infoLabel.show()
             return False
 
     def minimizeApp(self):
+        print "mini"
         if self.checkPwd():
             self.parent().isFullscreen = False
             self.parent().showMinimized()
-        self.close()
         
     def quitApp(self):
-        print "quitting?"
+        print "quit"
         if self.checkPwd():
-            self.parent().close()
+            QtGui.qApp.quit()
 
     def cancelDialog(self):
         self.close()
--- mainWindow.py
+++ mainWindow.py
@@ -2,19 +2,7 @@ from PyQt4 import QtCore,QtGui
 from mainWindow_ui import Ui_MainWindow
 import adminDialog
 import mpdclient2
-
-# This is a hack, to capture if the window loses focus (fx by alt-tab)
-# and if so, make the window stay fullscreen and not lose focus
-class EventFilter(QtCore.QObject):
-    def __init__(self, parent=None):
-        QtCore.QObject.__init__(self, parent)
-    def eventFilter(self, obj, event):
-        if event.type() == QtCore.QEvent.ActivationChange:
-            if not obj.ignoreFocusOut:
-                obj.isFullscreen = True
-                obj.showFullScreen()
-            obj.ignoreFocusOut = False
-        return QtCore.QObject.eventFilter(self, obj, event)
+import time
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
@@ -22,36 +10,26 @@ class MainWindow(QtGui.QMainWindow):
 
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
-        self.installEventFilter(EventFilter(self))
 
         self.mpd = mpdclient2.connect()
-        self.showFullScreen()
-        self.isFullscreen = True
-        self.ignoreFocusOut = False
+        self.oldPlaylist = -1
+        self.oldSongId = -99	# -1 means not playing
         self.startTimer(250)
+        
+        self.isFullscreen = False
+        #self.showFullScreen()
 
     def timerEvent(self, event):
-        self.updateUi()
+        if QtGui.qApp.activeWindow() == None and self.isFullscreen:
+            self.showFullScreen()
+        if QtGui.qApp.activeWindow() != None and not self.isFullscreen:
+            #self.isFullscreen = True
+            pass
+        self.periodicMpdCheck()
 
     def closeEvent(self, event):
         if self.isFullscreen:
             QtCore.QEvent.ignore(event)
-
-    def updateUi (self):
-        songInfo = self.mpd.currentsong()
-        if songInfo:
-            self.ui.curTitle.setText (songInfo['title'])
-            #self.ui.curArtist.setText (songInfo['artist'])
-        else:
-            self.ui.curTitle.setText ("Not playing")
-            self.ui.curArtist.setText ("Not playing")
-
-        currentSong = self.mpd.currentsong().pos
-        self.ui.playlist.clear()
-        for song in self.mpd.playlistinfo():
-            self.ui.playlist.addItem( str(int(song['pos'])+1) + '. ' + song['title'] )
-            if song['pos'] == currentSong:
-                self.ui.playlist.item(int(song['pos'])).setFont (QtGui.QFont("Arial", -1, QtGui.QFont.Bold))
 
     def enterAdmin(self):
         if self.isFullscreen:
@@ -61,5 +39,35 @@ class MainWindow(QtGui.QMainWindow):
             self.isFullscreen = True
             self.showFullScreen()
 
+    def periodicMpdCheck (self):
+        somethingChanged = False
+        # See if playlist changed
+        if self.oldPlaylist != int (self.mpd.status()['playlist']):
+            somethingChanged = True
+        # See if song changed
+        curSong = self.mpd.currentsong()
+        if curSong and int(curSong['id']) != self.oldSongId:
+            somethingChanged = True
+        elif not curSong and self.oldSongId != -1:
+            somethingChanged = True
 
+
+        if somethingChanged:
+            self.ui.playlist.clear()
+            for song in self.mpd.playlistinfo():
+                self.ui.playlist.addItem( str(int(song['pos'])+1) + '. ' \
+                        + song['artist'] + ' - ' + song['title'] )
+            self.oldPlaylist = int (self.mpd.status()['playlist'])
+
+            if curSong:
+                self.ui.curTitle.setText (curSong['title'])
+                self.ui.curArtist.setText (curSong['artist'])
+                curItem = self.ui.playlist.item(int(curSong['pos']))
+                curItem.setFont (QtGui.QFont("Arial", -1, QtGui.QFont.Bold))
+                self.ui.playlist.scrollToItem (curItem, QtGui.QAbstractItemView.PositionAtCenter)
+                self.oldSongId = int(curSong['id'])
+            else:
+                self.ui.curTitle.setText ("Not playing")
+                self.ui.curArtist.setText ("")
+                self.oldSongId = -1
 
