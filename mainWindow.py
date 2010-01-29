89452903cff7f0c76f48b353c8d10a8fd78232e7
Thomas Lundgaard <thomaslundgaard@gmail.com>
2010-01-29 03:16:07 +0100

    mpd class fully implemented

--- mainWindow.py
+++ mainWindow.py
@@ -15,10 +15,10 @@ class MainWindow(QtGui.QMainWindow):
         self.ui.setupUi(self)
 
         #Set UI styles TODO:Setting progressbar color doesnt work
-        tmppalette = self.ui.songProgress.palette()
-        tmppalette.setColor(QtGui.QPalette.Highlight, QtGui.QColor("red"))
-        tmppalette.setColor(QtGui.QPalette.Foreground, QtGui.QColor("red"))
-        self.ui.songProgress.setPalette(tmppalette)
+        #tmppalette = self.ui.songProgress.palette()
+        #tmppalette.setColor(QtGui.QPalette.Highlight, QtGui.QColor("red"))
+        #tmppalette.setColor(QtGui.QPalette.Foreground, QtGui.QColor("red"))
+        #self.ui.songProgress.setPalette(tmppalette)
 
         self.isFullscreen = False
         #self.showFullScreen()
@@ -26,6 +26,9 @@ class MainWindow(QtGui.QMainWindow):
         # Server & callbacks
         self.server = ServerInterface()
         self.server.sigStateChanged.connect(self.stateChanged)
+        self.server.sigSongChanged.connect(self.songChanged)
+        self.server.sigTimeChanged.connect(self.timeChanged)
+        self.server.sigPlaylistChanged.connect(self.playlistChanged)
 
     def timerEvent(self, event):
         if QtGui.qApp.activeWindow() == None and self.isFullscreen:
@@ -48,50 +51,30 @@ class MainWindow(QtGui.QMainWindow):
             self.showFullScreen()
 
     def stateChanged(self, state):
-        print "state blev ændret:", state
+        if state == 'stop':
+            self.ui.curTitle.setText ("Not playing")
+            self.ui.curArtist.setText ("")
 
-    def periodicMpdCheck (self):
-        somethingChanged = False
-        # See if playlist changed
-        if self.oldPlaylistId != int (self.mpd.status()['playlist']):
-            somethingChanged = True
-        # See if play status or song changed
-        if self.mpd.status()['state'] == "play":
-            if self.oldSongId == -1:
-                somethingChanged = True
-            if int (self.mpd.currentsong()['id']) != self.oldSongId:
-                somethingChanged = True
-        else:
-            if self.oldSongId != -1:
-                somethingChanged = True
+    def songChanged(self, songId):
+        if self.server.status()['state'] != 'stop':
+            curSong = self.server.currentsong()
+            self.ui.curTitle.setText (unicode(curSong['title'],"utf8"))
+            self.ui.curArtist.setText (unicode(curSong['artist'],"utf8"))
+            curItem = self.ui.playlist.item(int(curSong['pos']))
+            curItem.setFont (QtGui.QFont("Arial", -1, QtGui.QFont.Bold))
+            self.ui.playlist.scrollToItem (curItem, \
+                    QtGui.QAbstractItemView.PositionAtCenter)
 
-        if somethingChanged:
-            self.ui.playlist.clear()
-            for song in self.mpd.playlistinfo():
-                self.ui.playlist.addItem( str(int(song['pos'])+1) + '. ' \
-                        + unicode(song['artist'],"utf8") + ' - ' \
-                        + unicode(song['title'],"utf8") )
-            self.oldPlaylistId = int (self.mpd.status()['playlist'])
+    def timeChanged(self, elapsed, total):
+        txt = "%d:%02d/%d:%02d" % (elapsed/60, elapsed%60, total/60, total%60)
+        self.ui.songProgress.setValue (elapsed)
+        self.ui.songProgress.setMaximum (total)
+        self.ui.songProgress.setFormat(txt)
 
-            if self.mpd.status()['state'] == "play":
-                curSong = self.mpd.currentsong()
-                self.ui.curTitle.setText (unicode(curSong['title'],"utf8"))
-                self.ui.curArtist.setText (unicode(curSong['artist'],"utf8"))
-                curItem = self.ui.playlist.item(int(curSong['pos']))
-                curItem.setFont (QtGui.QFont("Arial", -1, QtGui.QFont.Bold))
-                self.ui.playlist.scrollToItem (curItem, \
-                        QtGui.QAbstractItemView.PositionAtCenter)
-                self.oldSongId = int(curSong['id'])
-            else:
-                self.ui.curTitle.setText ("Not playing")
-                self.ui.curArtist.setText ("")
-                self.oldSongId = -1
+    def playlistChanged(self, playlistId):
+        self.ui.playlist.clear()
+        for song in self.server.playlistinfo():
+            self.ui.playlist.addItem( str(int(song['pos'])+1) + '. ' \
+                    + unicode(song['artist'],"utf8") + ' - ' \
+                    + unicode(song['title'],"utf8") )
 
-        #Always update progress bar
-        try: 
-            timestr = self.mpd.status()['time']
-        except KeyError:
-            timestr = "-1:-1"
-        now, length = timestr.split(":")
-        now = str( int(now)/60 ) + ":" + str(int(now)%60)
-        length = str( int(length)/60 ) + ":" + str(int(length)%60)
--- serverInterface.py
+++ serverInterface.py
@@ -1,31 +1,26 @@
 # -*- coding: utf-8 -*-
 
-## Documentation ##
-# Signals:
-#   connected()
-
 from PyQt4 import QtCore
 from settings import Settings
 from mpd import *
 import socket
 
-
-class ServerInterface(QtCore.QObject):
+class ServerInterface(QtCore.QObject, MPDClient):
     sigConnected = QtCore.pyqtSignal()
     sigDisconnected = QtCore.pyqtSignal()
-    sigSongChanged = QtCore.pyqtSignal(int)
-    sigTimeChanged = QtCore.pyqtSignal(int)
-    sigPlaylistChanged = QtCore.pyqtSignal(int)
     sigStateChanged = QtCore.pyqtSignal(str)
+    sigSongChanged = QtCore.pyqtSignal(int)
+    sigTimeChanged = QtCore.pyqtSignal(int,int)     # (elapsed, total)
+    sigPlaylistChanged = QtCore.pyqtSignal(int)
 
     def __init__(self):
         QtCore.QObject.__init__(self)
+        MPDClient.__init__(self)
         self.settings = Settings()
-        self.mpd = MPDClient()
+        self.lastState=-9999
         self.lastSong=-9999
         self.lastTime=-9999
         self.lastPlaylist=-9999
-        self.lastState=-9999
         self.connect()
 
     def connect(self):
@@ -33,27 +28,43 @@ class ServerInterface(QtCore.QObject):
         port = self.settings.value("port")
         password = self.settings.value("password")
         try:
-            self.mpd.connect (host=server, port=port)
+            MPDClient.connect (self, host=server, port=port)
         except socket.error:
-            print "Unable to connect: Socket error (I will try again in 2 sec)"
+            print "Unable to connect to MPD: Socket error (I will try again in 2 sec)"
             QtCore.QTimer.singleShot(2000, self.connect )
             return False
         if password != "":
             try:
-                self.mpd.password(password)
+                self.password(password)
             except CommandError:
-                print "Unable to connect: Invalid password"
+                print "Unable to connect to MPD: Invalid password"
                 return False
         self.sigConnected.emit()
-        self.startTimer(250)
+        self.startTimer(500)
         return True
 
     def timerEvent(self, event):
-        status = self.mpd.status()
+        try:
+            status = self.status()
+        except socket.error:
+            print "Lost connection to MPD. Trying to reconnect"
+            self.sigDisconnected.emit()
+            self.stopTimer()
+            self.disconnect()
+            self.connect()
+            return
+        if status['playlist'] != self.lastPlaylist or \
+         status['songid'] != self.lastSong:
+            self.sigPlaylistChanged.emit(int(status['playlist']))
+            self.lastPlaylist = status['playlist']
+        if status['songid'] != self.lastSong:
+            self.sigSongChanged.emit(int(status['songid']))
+            self.lastSong = status['songid']
+        if 'time' in status and status['time'] != self.lastTime:
+            elapsed, total = status['time'].split(":")
+            self.sigTimeChanged.emit(int(elapsed), int(total))
+            self.lastTime = status['time']
         if status['state'] != self.lastState:
             self.sigStateChanged.emit(status['state'])
             self.lastState = status['state']
-        if status['songid'] != self.lastSong:
-            self.sigSongChanged.emit(status['songid'])
-            self.lastSong = status['songid']
-        if s
+
