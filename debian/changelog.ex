03943145491bf2572b85191c8b773ce7c6b0bbbf
peter <peter@peter-ubuntu.(none)>
2010-02-16 20:41:32 +0100

    Added files for debian package building

---
+++ debian/changelog.ex
@@ -0,0 +1,7 @@
+pympdjuke (1.0beta1-1) unstable; urgency=low
+
+   * Initial release (Closes: #nnnn)  <nnnn is the bug number of your ITP>
+
+ -- peter <peterbjorgensen@gmail.com>  Tue, 16 Feb 2010 18:44:48 +0100
+
+*Example file to show syntax for bug fixes
