b89dbc7af6e011cdc22cb5d217acc6e53c772311
Thomas Lundgaard <thomaslundgaard@gmail.com>
2010-02-25 12:16:04 +0100

    added license / copyright notice

--- src/adminDialog.py
+++ src/adminDialog.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# PyMpdJuke - A mpd-frontend to be used as a jukebox at parties.
+# Copyright (C) 2010 Peter Bjørn
+# Copyright (C) 2010 Thomas Lundgaard
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from PyQt4 import QtGui, QtCore
 from adminDialog_ui import Ui_AdminDialog
 from virtualKeyboard import VirtualKeyboard
